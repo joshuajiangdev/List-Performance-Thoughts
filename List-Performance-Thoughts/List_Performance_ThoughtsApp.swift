8dca1c49d9ca7973204ad4fdfb29fac3d036e3ca
Joshua Jiang <zhuoshengjiang@gmail.com>
2022-10-30 02:37:03 -0400

    Initial Commit

---
+++ List-Performance-Thoughts/List_Performance_ThoughtsApp.swift
@@ -0,0 +1,17 @@
+//
+//  List_Performance_ThoughtsApp.swift
+//  List-Performance-Thoughts
+//
+//  Created by Joshua Jiang on 10/30/22.
+//
+
+import SwiftUI
+
+@main
+struct List_Performance_ThoughtsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
